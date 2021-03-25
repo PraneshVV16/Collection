76d3223693d8ebbff604edef03fbfebc5774eeba
PraneshVV16 <80122069+PraneshVV16@users.noreply.github.com>
2021-03-25 14:16:11 +0530

    Add files via upload

---
+++ src/treeset/TreesetExample.java
@@ -0,0 +1,19 @@
+package treeset;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+class TreeSetExample{
+    public static void main(String args[]){
+
+        TreeSet<String> al=new TreeSet<String>();
+        al.add("Ravi");
+        al.add("Vijay");
+        al.add("Ravi");
+        al.add("Ajay");
+        
+        Iterator<String> itr=al.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
